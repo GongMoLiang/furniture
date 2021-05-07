8f7ed970f254b58e619d4db66fb5ee8105d4a025
GongMoLiang <gml18720826651@163.comm>
2021-05-07 21:34:53 +0800

    prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -41,9 +41,5 @@ module.exports = {
 
 	jsxBracketSameLine: false,
 
-<<<<<<< HEAD
 	elsintIntergration: true,
-=======
-	elsintIntergration: true
->>>>>>> c30875c50edbc16227437fd3cff1cada4d823c6b
 };
